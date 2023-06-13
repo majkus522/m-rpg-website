6fa6e737c3e9ae3afb6231faa8771db8cadfeb17
majkus522 <mikohus522@gmail.com>
2023-06-13 20:47:29 +0200

    Login redesign 13.06

--- login/scripts/main.js
+++ login/scripts/main.js
@@ -1,17 +1,26 @@
 let button = document.querySelector("form input[type='button']");
-button.disabled = true;
+let error = document.querySelector("form p.error");
 button.addEventListener("click", () =>
 {
     let inputs = document.querySelectorAll("form > label > input");
     let username = inputs[0].value;
+    if(username.length == 0)
+    {
+        error.textContent = "Enter username";
+        return;
+    }
     let password = btoa(inputs[1].value);
+    if(password.length == 0)
+    {
+        error.textContent = "Enter password";
+        return;
+    }
 
     let request = new XMLHttpRequest();
     request.open("GET", "../api/endpoints/players/" + username + "/logged");
     request.onload = function ()
     {
         let response = JSON.parse(this.responseText);
-        let error = document.querySelector("form p.error");
         if(this.status >= 400)
         {
             error.textContent = response.message;
@@ -21,8 +30,8 @@ button.addEventListener("click", () =>
             if(response)
             {
                 let remember = document.querySelector("form input[type='checkbox']").value;
-                document.cookie = "username=" + username + ";path=/" + (!remember ? (";max-age=" + (60)) : "");
-                document.cookie = "password=" + password + ";path=/" + (!remember ? (";max-age=" + (60)) : "") + ";secure";
+                document.cookie = "username=" + username + ";path=/" + (!remember ? (";max-age=" + (60 * 60 * 24)) : "");
+                document.cookie = "password=" + password + ";path=/" + (!remember ? (";max-age=" + (60 * 60 * 24)) : "") + ";secure";
                 location.reload();
             }
             else
@@ -33,21 +42,4 @@ button.addEventListener("click", () =>
     };
     request.setRequestHeader("Password", password);
     request.send();
-});
-let inputsTyped = [false, false];
-document.querySelector("form input[type='text']").addEventListener("input", (event) =>
-{
-    if(event.target.value.length > 0)
-        inputsTyped[0] = true;
-    else
-        inputsTyped[0] = false;
-    button.disabled = !(inputsTyped[0] && inputsTyped[1]);
-});
-document.querySelector("form input[type='password']").addEventListener("input", (event) =>
-{
-    if(event.target.value.length > 0)
-        inputsTyped[1] = true;
-    else
-        inputsTyped[1] = false;
-    button.disabled = !(inputsTyped[0] && inputsTyped[1]);
 });
