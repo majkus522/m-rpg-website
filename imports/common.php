439dfad00b8cf0434d5bd782c76c89906d5c812b
majkus522 <mikohus522@gmail.com>
2023-06-23 16:04:11 +0200

    User security 23.06

--- imports/common.php
+++ imports/common.php
@@ -77,29 +77,39 @@
         return $headers;
     }
 
-    function validPassword($password):bool|string
+    function validPassword(string $password):bool|string
     {
         $password = trim($password);
         if(strlen($password) < 6)
-        {
             return "Password must be at least 6 characters long";
-        }
         if(!preg_match("@[A-Z]@", $password))
-        {
             return "Password must contain at least one large character";
-        }
         if(!preg_match("@[a-z]@", $password))
-        {
             return "Password must contain at least one small character";
-        }
         if(!preg_match("@[0-9]@", $password))
-        {
             return "Password must contain at least one number";
-        }
         if(!preg_match("@[^\w]@", $password))
-        {
             return "Password must contain at least one special character";
-        }
+        return true;
+    }
+
+    function validUsername(string $username):bool|string
+    {
+        $username = trim($username);
+        if(strlen($username) < 3)
+            return "Username is to short";
+        if(strlen($username) > 16)
+            return "Username is to long";
+        if(preg_match("[^a-zA-Z0-9_]"))
+            return "Username can only contain letters, numbers and underscore";
+        return true;
+    }
+
+    function validEmail(string $email):bool|string
+    {
+        $email = trim($email);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+            return "Incorect email";
         return true;
     }
 ?>
