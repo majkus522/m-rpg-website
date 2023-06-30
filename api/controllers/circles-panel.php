8e1e7082d99259f4d8278265a96073fb5222fe85
majkus522 <mikohus522@gmail.com>
2023-06-30 20:07:28 +0200

    API circles panel fix 30.06

--- api/controllers/circles-panel.php
+++ api/controllers/circles-panel.php
@@ -1,20 +1,17 @@
 <?php
-    $header = getHeader("Items");
-    $limit = 50;
-    $offset = 0;
-    if($header != false)
+    switch($requestMethod)
     {
-        if(str_contains($header, "-"))
-        {
-            $part = explode("-", $header);
-            $offset = (int)$part[0];
-            $limit = (int)$part[1];
-        }
-        else
-            $limit = (int)$header;
-    }
-    $player = 1;
-    $query = 'select `circles`.`name`, `circles`.`slug`, (select count(`circles-likes`.`player`) from `circles-likes` where `circles-likes`.`circle` = `circles`.`id`) as "likes", ifnull((select (`circles-likes`.`player` = ' . $player . ') from `circles-likes` where `circles-likes`.`circle` = `circles`.`id` group by `circles-likes`.`circle`), 0) as "liked" from `circles` left join `players` on `players`.`id` = `circles`.`player` where !`circles`.`private` limit ' . $limit . ' offset ' . $offset;
-    $queryResult = connectToDatabase($query);
-    echo json_encode($queryResult);
+        case "GET":
+            $player = 1;
+            $query = 'select `circles`.`name`, `circles`.`slug`, (select count(`circles-likes`.`player`) from `circles-likes` where `circles-likes`.`circle` = `circles`.`id`) as "likes", ifnull((select (`circles-likes`.`player` = ' . $player . ') from `circles-likes` where `circles-likes`.`circle` = `circles`.`id` group by `circles-likes`.`circle`), 0) as "liked" from `circles` left join `players` on `players`.`id` = `circles`.`player` where !`circles`.`private`';
+            require "headerItems.php";
+            $query .= ' limit ' . $limit . ' offset ' . $offset;
+            $queryResult = connectToDatabase($query);
+            echo json_encode($queryResult);
+            break;
+
+        default:
+            exitApi(501, "Method not implemented");
+            break;
+    } 
 ?>
