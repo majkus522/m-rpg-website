c35c24d46cfeb5b4fe3825b385317d363fadf4f3
majkus522 <mikohus522@gmail.com>
2023-07-10 16:30:03 +0200

    API circles redesing 10.07

--- api/endpoints/circles.php
+++ api/endpoints/circles.php
@@ -1,43 +1,40 @@
 <?php
+    require "playerLogged.php";
+
     switch($requestMethod)
     {
-        /*case "GET":
-            $headerPlayer = getHeader("Player");
-            $headerPassword = getHeader("Password");
+        case "GET":
             if(isSingleGet())
             {
-                $query = 'select * from `circles` where `slug` = "' . $requestUrlPart[$urlIndex + 1] . '" limit 1';
+                $query = 'select * from `circles` where (!`private`';
+                $header = getHeader("Player");
+                if($header !== false)
+                {
+                    $login = isPlayerLogged($header);
+                    if($login === true)
+                    {
+                        $query .= ' or `player` = (select `player` from `players-sessions` where `key` = "' . getHeader("Session-Key") . '" limit 1)';
+                    }
+                }
+                $query .= ') and `slug` = "' . $requestUrlPart[$urlIndex + 1] . '" limit 1';
                 $queryResult = connectToDatabase($query);
                 if(empty($queryResult))
                     exitApi(404, "Circle doesn't exists");
-                $circle = $queryResult[0];
-                if($circle->private)
-                {
-                    if($headerPlayer == false)
-                        exitApi(400, "Specify Player header");
-                    if($headerPassword == false)
-                        exitApi(400, "Specify Password header");
-                    $query = 'select `username` from `players` where `id` = ' . $circle->player . ' limit 1';
-                    $queryResult = connectToDatabase($query);
-                    if(empty($queryResult))
-                        exitApi(500, "Circle without creator");
-                    if($queryResult[0]->username != $headerPlayer)
-                        exitApi(403, "You can't see someones private circle");
-                    if(!callApi("endpoints/players/" . $headerPlayer . "/logged", "GET", ["Password: " . $headerPassword])[0])
-                        exitApi(401, "Wrong password");
-                }
-                $result = $circle;
+                $result = $queryResult[0];
             }
             else
             {
-                if($headerPlayer != false && $headerPassword != false)
+                $query = 'select * from `circles` where (!`private`';
+                $header = getHeader("Player");
+                if($header !== false)
                 {
-                    if(!callApi("endpoints/players/" . $headerPlayer . "/logged", "GET", ["Password: " . $headerPassword])[0])
-                        exitApi(401, "Wrong password");
-                    $query = 'select * from `circles` where (!`private` or `player` = (select `id` from `players` where `username` = "' . $headerPlayer . '"))';
+                    $login = isPlayerLogged($header);
+                    if($login === true)
+                    {
+                        $query .= ' or `player` = (select `player` from `players-sessions` where `key` = "' . getHeader("Session-Key") . '" limit 1)';
+                    }
                 }
-                else
-                    $query = 'select * from `circles` where !`private`';
+                $query .= ')';
                 $order = "";
                 foreach($_GET as $key => $value)
                 {
@@ -74,33 +71,17 @@
                 }
                 require "headerItems.php";
                 $query .= $order . ' limit ' . $limit . ' offset ' . $offset;
-                $result = connectToDatabase($query);
-                if(empty($result))
-                    exitApi(404, "Can't find any circles matching conditions");
-                header("Items-Count: " . sizeof($result));
+                $queryResult = connectToDatabase($query);
+                if(empty($queryResult))
+                    exitApi(404, "Can't find any circle matching conditions");
+                header("Items-Count: " . sizeof($queryResult));
+                $result = $queryResult;
             }
             echo json_encode($result);
             break;
 
-        case "OPTIONS":
-            echo json_encode([
-                "GET /api/endpoints/circles = select all circles (hidden data), doeasn't show private ones (player don't logged)",
-                "   Items: {offset(optional)}-{limit} = specify number and offset of selected players (default[Items: 0-50])",
-                "   ?minMana={minMana} = specify minimal mana usage of circle",
-                "   ?maxMana={maxMana} = specify maxminal mana usage of circle",
-                "GET /api/endpoints/circles = select all circles (hidden data), doeasn't show private ones (player logged)",
-                "   (required)Player: {username} = specify player username",
-                "   (required)Passowrd: {password} = send player password",
-                "   Apply all parameters from endpoint above",
-                "GET /api/endpoints/circles/{slug} = select circle with specified slug, doeasn't show private ones (player don't logged)",
-                "GET /api/endpoints/circles/{slug} = select circle with specified slug, doeasn't show other players private ones (player logged)",
-                "   (required)Player: {username} = specify player username",
-                "   (required)Passowrd: {password} = send player password"
-            ]);
-            break;
-
         default:
             exitApi(501, "Method not implemented");
-            return;*/
+            return;
     }
 ?>
