9a6e31e5ddb58c264f257b26e20a17fac2f8d883
majkus522 <mikohus522@gmail.com>
2023-07-21 22:44:30 +0200

    Players panel stats 21.07

--- players/panel.php
+++ players/panel.php
@@ -18,12 +18,51 @@
                         echo $result[0]->username;
                     ?></h3>
                     <div>
+                        <div>
+                            <stat>
+                                <p>Strength</p>
+                                <p><?php
+                                    echo $result[0]->strength;
+                                ?></p>
+                            </stat>
+                            <stat>
+                                <p>Agility</p>
+                                <p><?php
+                                    echo $result[0]->agility;
+                                ?></p>
+                            </stat>
+                            <stat>
+                                <p>Charisma</p>
+                                <p><?php
+                                    echo $result[0]->charisma;
+                                ?></p>
+                            </stat>
+                            <stat>
+                                <p>Intelligence</p>
+                                <p><?php
+                                    echo $result[0]->intelligence;
+                                ?></p>
+                            </stat>
+                        </div>
+                        <graph>
+                            <svg>
+                                <polygon/>
+                                <line/>
+                                <line/>
+                            </svg>
+                            <p>STR</p>
+                            <p>AGL</p>
+                            <p>CHR</p>
+                            <p>INT</p>
+                        </graph>
+                    </div>
+                    <stat>
                         <p>Level</p>
                         <p><?php
                             echo $result[0]->level;
                         ?></p>
-                    </div>
-                    <div>
+                    </stat>
+                    <stat>
                         <bar><fill style="width: <?php
                             echo round($result[0]->exp / levelExp($result[0]->level) * 100, 2);
                         ?>%;"></fill></bar>
@@ -31,13 +70,13 @@
                         <p><?php
                             echo $result[0]->exp . " / " . levelExp($result[0]->level);
                         ?></p>
-                    </div>
-                    <div>
+                    </stat>
+                    <stat>
                         <p>Money</p>
                         <p><?php
                             echo $result[0]->money;
                         ?> $</p>
-                    </div>
+                    </stat>
                 </info>
             </content>
         </main>
@@ -46,3 +85,10 @@
         ?>
     </body>
 </html>
+<script>
+    let str = <?php echo $result[0]->strength; ?>;
+    let agl = <?php echo $result[0]->agility; ?>;
+    let chr = <?php echo $result[0]->charisma; ?>;
+    let int = <?php echo $result[0]->intelligence; ?>;
+</script>
+<script src="scripts/panel.js"></script>
---
+++ players/scripts/panel.js
@@ -0,0 +1,26 @@
+window.addEventListener("resize", draw);
+
+function draw()
+{
+    let svg = document.querySelector("info svg");
+    let rect = svg.getBoundingClientRect();
+    let heigth = rect.height;
+    let width = heigth;
+    svg.setAttribute("width", heigth);
+    let max = Math.max(str, agl, chr, int);
+    svg.querySelector("polygon").setAttribute("points", (width / 2) + "," + ((heigth / 2) - (heigth / 2 * str / max) + 5) + " " + (width - ((width / 2) - (width / 2 * agl / max)) - 5) + "," + (heigth / 2) + " " + (width / 2) + "," + (heigth - ((heigth / 2) - (heigth / 2 * chr / max)) - 5) + " " + ((width / 2) - (width / 2 * int / max) + 5) + "," + (heigth / 2));
+
+    let line = svg.querySelectorAll("line")[0];
+    line.setAttribute("x1", 0);
+    line.setAttribute("y1", heigth / 2);
+    line.setAttribute("x2", width);
+    line.setAttribute("y2", heigth / 2);
+
+    line = svg.querySelectorAll("line")[1];
+    line.setAttribute("x1", width / 2);
+    line.setAttribute("y1", 0);
+    line.setAttribute("x2", width / 2);
+    line.setAttribute("y2", heigth);
+}
+
+draw();
