d1e52105c598d2f20478249296d7d0d41bb97b3a
majkus522 <mikohus522@gmail.com>
2023-08-03 16:20:20 +0200

    Revert 3e08cf2

---
+++ api/mailer.php
@@ -0,0 +1,38 @@
+<?php
+    function sendMail($to, $subject, $message)
+    {
+        require "https://raw.githubusercontent.com/PHPMailer/PHPMailer/master/src/Exception.php";
+        require "https://raw.githubusercontent.com/PHPMailer/PHPMailer/master/src/PHPMailer.php";
+        require "https://raw.githubusercontent.com/PHPMailer/PHPMailer/master/src/SMTP.php";
+        require "mailerConfig.php";
+    
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+    
+        $mail = new PHPMailer(true);
+        try
+        {
+            $mail->isSMTP();
+            $mail->Host = $host;
+            $mail->SMTPAuth = true;
+            $mail->Username = $username;
+            $mail->Password = $password;
+            $mail->SMTPSecure = "ssl";
+            $mail->Port = 465;
+            
+            $mail->setFrom($username, "M-RPG");
+            $mail->addAddress($to);
+            
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $message;
+    
+            $mail->send();
+        }
+        catch (Exception $e)
+        {
+            exitApi(500, "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        }
+        $mail->smtpClose();
+    }
+?>
