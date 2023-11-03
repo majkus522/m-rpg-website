218851bead2f1127a5898c4e5d6f1869dd17a72f
majkus522 <mikohus522@gmail.com>
2023-11-03 20:38:40 +0100

    Moved to majkus522/m-rpg-utils@769d41e89c145eb7d4ff26abf27388f92c950467

--- mysql/functions/skill-label.sql
+++
@@ -1,7 +0,0 @@
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` FUNCTION `skill_label`(`root` TEXT CHARSET utf8, `file` TEXT CHARSET utf8) RETURNS TEXT CHARSET utf8 COLLATE utf8_bin
-    DETERMINISTIC
-begin
-    return replace(json_extract(convert(LOAD_FILE(concat(root, "/", file, ".json")) using utf8mb4), "$.label"), '"', "");
-end$$
-DELIMITER ;
