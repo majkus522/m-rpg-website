5b8b7382dbb4cecfc990ad80b6579f2738599ac0
majkus522 <mikohus522@gmail.com>
2023-12-06 18:26:55 +0100

    Auto delete empty guilds

--- api/cron.php
+++ api/cron.php
@@ -2,4 +2,5 @@
     require "databaseController.php";
     connectToDatabase('delete from `players-sessions` where `temp` and hour(timediff(now(), `date`)) > 24');
     connectToDatabase('delete from `password-recovery` where hour(timediff(now(), `date`)) > 24');
+    connectToDatabase('delete from `guilds` where (select count(`players`.`id`) from `players` where `guild` = `guilds`.`id`) = 0');
 ?>
