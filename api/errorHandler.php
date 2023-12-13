96913b03eed395e945b1181ed966dc9d589d167e
majkus522 <mikohus522@gmail.com>
2023-12-13 18:52:27 +0100

    Error handling update

--- api/errorHandler.php
+++ api/errorHandler.php
@@ -1,52 +1,24 @@
 <?php
-    function errorHandler():void
+    register_shutdown_function(function ()
     {
-        global $requestMethod;
-        if (error_reporting())
+        $error = error_get_last();
+        if($error != null)
         {
-            http_response_code(500);
-            $values = func_get_args();
-            while(sizeof($values) > 4)
-                array_pop($values);
-            array_shift($values);
-            $values[1] = str_replace("\\", "/", $values[1]);
-            $response = array_combine(["message", "file", "line"], $values);
-            $result = json_encode($response);
-            header("Content-Length: " . strlen($result));
-            if($requestMethod != "HEAD")
-                echo $result;
+            unset($error["type"]);
+            echo json_encode($error);
+            databaseClose();
             die();
         }
-        databaseClose();
-    }
-
-    function parseHandler():void
+    });
+    set_error_handler(function ($error_level, $error_message, $error_file, $error_line)
     {
-        global $requestMethod;
-        $lasterror = error_get_last();
-        if ( $lasterror == null )
-            return;
-        http_response_code(500);
-        if(str_starts_with($lasterror["message"], "Uncaught Exception: "))
-        {
-            $message = str_replace("Uncaught Exception: ", "", $lasterror["message"]);
-            $part = explode(".", $message);
-            $partFile = explode(" ", $part[0]);
-            $file = end($partFile) . ".php";
-            $line = str_replace("php:", "", explode("\n", $part[1])[0]);
-            $response = array_combine(["message", "file", "line"], [explode(":", str_replace(" in " . $file, "", $message))[0], $file, $line]);
-        }
-        else
-            $response = array_combine(["code", "message", "file", "line"], [$lasterror['type'], $lasterror['message'], str_replace("\\", "/", $lasterror['file']), $lasterror['line']]);
-        $result = json_encode($response);
-        header("Content-Length: " . strlen($result));
-        if($requestMethod != "HEAD")
-            echo $result;
+        echo json_encode([
+            "message" => $error_message,
+            "file" => $error_file,
+            "line" => $error_line
+        ]);
         databaseClose();
         die();
-    }
-
+    });
     ini_set('display_errors', false);
-    set_error_handler("errorHandler", E_ALL);
-    register_shutdown_function("parseHandler");
 ?>
