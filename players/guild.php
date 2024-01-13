7eddc58fb16f9bfd2bedb25edbdfb17b83f09776
majkus522 <mikohus522@gmail.com>
2024-01-13 23:32:59 +0100

    API guilds implement vice leader

--- players/guild.php
+++ players/guild.php
@@ -1,12 +1,11 @@
 <?php
     $resultPlayer = callApi("players/$_COOKIE[username]", "GET", ["Session-Key: " . $_COOKIE["session-key"], "Session-ID: " . $_COOKIE["session-id"]])->content;
     $resultGuild = callAPi("guilds/$resultPlayer->guild", "GET");
-    $isLeader = $resultGuild->content->leader == $resultPlayer->id;
+    $permissionLevel = $resultGuild->content->leader == $resultPlayer->id ? 2 : ($resultGuild->content->vice_leader == $resultPlayer->id ? 1 : 0);
 ?>   
         <title>M-RPG - <?php echo $_COOKIE["username"]; ?> - Guild</title>
         <script>
             let guild = "<?php echo $resultPlayer->guild; ?>";
-            let isLeader = <?php echo $isLeader ? "true" : "false"; ?>;
         </script>
         <script src="../imports/scripts.js" defer></script>
         <script src="scripts/guild.js" defer></script>
@@ -26,15 +25,17 @@
                         $index = 1;
                         foreach($resultMembers->content as $element)
                         {
-                            $fsdf = "";
+                            $line = $element->username;
+                            if($element->type != "member")
+                                $line .= "  ( " . ucwords(str_replace("_", " ", $element->type)) . " )";
                             echo <<< END
                     <tr>
                         <td>$index</td>
-                        <td>$element</td>
+                        <td>$line</td>
                         <td>
 END;
-                            if($isLeader && $element != $resultPlayer->username)
-                                echo "<button data-player=\"$element\">Kick</button>";
+                            if($permissionLevel >= 1 && $element->username != $resultPlayer->username && $element->type != "leader")
+                                echo "<button data-player=\"$element->username\">Kick</button>";
                             echo <<< END
 </td>
                     </tr>
@@ -46,15 +47,26 @@ END;
                 </table>
                 <div>
                     <h2><?php echo $resultGuild->content->name; ?></h2>
-                    <p>Leader: <?php echo $resultMembers->content[0]; ?></p>
+                    <p>Leader: <?php
+                        $filter = "leader";
+                        echo array_values(array_filter($resultMembers->content, "filterMember"))[0]->username;
+                    ?></p><?php
+                        $filter = "vice_leader";
+                        $viceLeader = array_values(array_filter($resultMembers->content, "filterMember"));
+                        if(!empty($viceLeader))
+                            echo "\r\n\t\t\t\t\t<p>Vice Leader: " . $viceLeader[0]->username . "</p>";
+                    ?>
+
                     <p>Members: <?php echo $resultMembers->headers["Return-Count"]; ?></p>
-                    <button>Leave guild</button>
+                    <button data-operation="leave">Leave guild</button>
 <?php
-                        if($isLeader)
+                        if($permissionLevel == 2)
                         {
-                            echo "\t\t\t\t\t<button>Delete guild</button>\r\n";
-                            echo "\t\t\t\t\t<button>Select new leader</button>\r\n";
+                            echo "\t\t\t\t\t<button data-operation=\"delete\">Delete guild</button>\r\n";
+                            echo "\t\t\t\t\t<button data-operation=\"leader\">Select new leader</button>\r\n";
                         }
+                        if($permissionLevel >= 1)
+                            echo "\t\t\t\t\t<button data-operation=\"vice\">Select new vice leader</button>\r\n";
                     ?>
                     <p class="error"></p>
                 </div>
@@ -65,3 +77,10 @@ END;
         ?>
     </body>
 </html>
+<?php
+    function filterMember($input)
+    {
+        global $filter;
+        return $input->type == $filter;
+    }
+?>
--- players/scripts/guild.js
+++ players/scripts/guild.js
@@ -3,21 +3,47 @@ let modeKick = true;
 
 reloadButtons(kickPlayer, "Kick");
 
-let controls = document.querySelectorAll("content div button");
-controls[0].addEventListener("click", (event) => request("PATCH", "../api/players/" + getCookie("username") + "/leave"));
-
-if(controls.length > 1)
+document.querySelectorAll("content div button").forEach(element =>
 {
-    controls[1].addEventListener("click", (event) => request("DELETE", "../api/guilds/" + guild));
-    controls[2].addEventListener("click", (event) =>
+    element.addEventListener("click", (event) =>
     {
-        modeKick = !modeKick;
-        event.target.textContent = modeKick ? "Select new leader" : "Kick player";
-        if(modeKick)
-            reloadButtons(kickPlayer, "Kick");
-        else
-            reloadButtons(newLeader, "New leader");
+        switch(event.target.dataset.operation)
+        {
+            case "leave":
+                request("PATCH", "../api/guilds/" + guild + "/kick", getCookie("username"));
+                break;
+
+            case "delete":
+                request("DELETE", "../api/guilds/" + guild);
+                break;
+
+            case "leader":
+                reloadButtons(newLeader, "New leader");
+                createKick();
+                break;
+
+            case "vice":
+                reloadButtons(newViceLeader, "New vice leader");
+                createKick();
+                break;
+        }
     });
+});
+
+function createKick()
+{
+    let main = document.querySelector("content div");
+    if(main.querySelector('button[data-operation="kick"]') != null)
+        return;
+    let button = document.createElement("button");
+    button.textContent = "Kick player";
+    button.dataset.operation = "kick";
+    button.addEventListener("click", (event) =>
+    {
+        event.target.remove();
+        reloadButtons(kickPlayer, "Kick");
+    });
+    main.insertBefore(button, main.querySelector("p.error"));
 }
 
 function request(method, url, body = "")
@@ -38,12 +64,19 @@ function request(method, url, body = "")
 
 function reloadButtons(action, text)
 {
-    document.querySelectorAll("table button").forEach((element) =>
+    document.querySelectorAll("table tr").forEach((element) =>
     {
-        element.removeEventListener("click", kickPlayer);
-        element.removeEventListener("click", newLeader);
-        element.addEventListener("click", action);
-        element.textContent = text;
+        let button = element.querySelector("button");
+        if(button == null)
+            return;
+        button.style.display = "block";
+        if(text == "New vice leader" && element.querySelector("td:nth-child(2)").textContent.includes("Vice Leader"))
+            button.style.display = "none";
+        button.removeEventListener("click", kickPlayer);
+        button.removeEventListener("click", newLeader);
+        button.removeEventListener("click", newViceLeader);
+        button.addEventListener("click", action);
+        button.textContent = text;
     });
 }
 
@@ -55,4 +88,9 @@ function kickPlayer(event)
 function newLeader(event)
 {
     request("PATCH", "../api/guilds/" + guild, JSON.stringify({ leader: event.target.dataset.player}));
+}
+
+function newViceLeader(event)
+{
+    request("PATCH", "../api/guilds/" + guild, JSON.stringify({ vice_leader: event.target.dataset.player}));
 }
